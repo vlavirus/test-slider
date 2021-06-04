040f81c5c47326b079c3ca5f53c2e8681a78a090
vlavirus <androando777@gmail.com>
2021-06-04 14:03:57 +0300

    feat(app): add json db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Card } from './shared/models/card.model';
+import * as data from '../db.json';
 
 @Component({
   selector: 'app-root',
@@ -9,68 +10,5 @@ import { Card } from './shared/models/card.model';
 })
 export class AppComponent {
   title = 'slider-test';
-  cards: Card[] = [
-    {
-      cardImage: 'assets/images/avatars/user1.png',
-      cardUserName: 'Nick Colebatch',
-      cardUserPosition: 'RV Cover Supply',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying Google Analytics data which can be demystifying Google Analytics data which can be'
-    },
-    {
-      cardImage: 'assets/images/avatars/user2.png',
-      cardUserName: 'Carlene Grace',
-      cardUserPosition: '2019 Verified Google 5-Star Review',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying ' +
-        'Google Analytics data which can be overwhelming. The service trawls through' +
-        ' your data and finds the areas where you can improve - even if you were an expert' +
-        ' this would save time. I\'ve found Diib to be very helpful on demystifying Google Analytics' +
-        ' data which can be overwhelming. The service trawls through your data and finds the areas where ' +
-        'you can improve - even if you were an expert this would save time. For me...'
-    },
-    {
-      cardImage: 'assets/images/avatars/user2.png',
-      cardUserName: 'Carlene Grace',
-      cardUserPosition: '2019 Verified Google 5-Star Review',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying ' +
-        'Google Analytics data which can be overwhelming. The service trawls through' +
-        ' your data and finds the areas where you can improve - even if you were an expert' +
-        ' this would save time. I\'ve found Diib to be very helpful on demystifying Google Analytics' +
-        ' data which can be overwhelming. The service trawls through your data and finds the areas where ' +
-        'you can improve - even if you were an expert this would save time. For me...'
-    },
-    {
-      cardImage: 'assets/images/avatars/user2.png',
-      cardUserName: 'Carlene Grace',
-      cardUserPosition: '2019 Verified Google 5-Star Review',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying ' +
-        'Google Analytics data which can be overwhelming. The service trawls through' +
-        ' your data and finds the areas where you can improve - even if you were an expert' +
-        ' this would save time. I\'ve found Diib to be very helpful on demystifying Google Analytics' +
-        ' data which can be overwhelming. The service trawls through your data and finds the areas where ' +
-        'you can improve - even if you were an expert this would save time. For me...'
-    },
-    {
-      cardImage: 'assets/images/avatars/user2.png',
-      cardUserName: 'Carlene Grace',
-      cardUserPosition: '2019 Verified Google 5-Star Review',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying ' +
-        'Google Analytics data which can be overwhelming. The service trawls through' +
-        ' your data and finds the areas where you can improve - even if you were an expert' +
-        ' this would save time. I\'ve found Diib to be very helpful on demystifying Google Analytics' +
-        ' data which can be overwhelming. The service trawls through your data and finds the areas where ' +
-        'you can improve - even if you were an expert this would save time. For me...'
-    },
-    {
-      cardImage: 'assets/images/avatars/user2.png',
-      cardUserName: 'Carlene Grace',
-      cardUserPosition: '2019 Verified Google 5-Star Review',
-      cardUserDescription: 'I\'ve found Diib to be very helpful on demystifying ' +
-        'Google Analytics data which can be overwhelming. The service trawls through' +
-        ' your data and finds the areas where you can improve - even if you were an expert' +
-        ' this would save time. I\'ve found Diib to be very helpful on demystifying Google Analytics' +
-        ' data which can be overwhelming. The service trawls through your data and finds the areas where ' +
-        'you can improve - even if you were an expert this would save time. For me...'
-    },
-
-  ]
+  cards: Card[] = JSON.parse(JSON.stringify(data)).default.users;
 }
